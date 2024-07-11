7ebb24c4f8dff0eb52cb08ca53260cd8e855cb15
Yash KC <yashchauhan.coder@gmail.com>
2024-07-11 20:50:43 +0530

    added constants

---
+++ src/constants/colors.js
@@ -0,0 +1,15 @@
+export const bgcolorHex = () => {
+    return "#1C1C1C";
+}
+
+export const textPrimaryColor = () => {
+    return "rgba(255,255,255,1)"
+}
+
+export const colorsBG = {
+    black: "black",
+    primary: "#1d1d1d",
+    secondary: "#252525",
+    neonSecondary: "#03d6d2",
+    neonTertiary: "#006a68"
+}
